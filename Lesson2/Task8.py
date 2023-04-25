ff2ba5a5cf061bc740021d07e3578079af576a70
Sana <Yes>
2023-04-25 12:24:34 +0500

    repository reorganization

--- Lesson2/Task8.py
+++ Lesson2/Task8.py
@@ -4,11 +4,12 @@ x = int(input("Введи первое число: "))
 y = int(input("Введи второе число: "))
 z = int(input("Введи третье число: "))
 
-if x > y:
-    g_max = x
-else:
-    g_max = y    
-if g_max > z:
-    print(g_max)
-else:
-    print(z)
+#print(max(x, y, z))    
+# if x > y:
+#     g_max = x
+# else:
+#     g_max = y    
+# if g_max > z:
+#     print(g_max)
+# else:
+#     print(z)
